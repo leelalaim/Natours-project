02bcef6a1c2aeb3493003808f582dab105463a60
Laima Duhovnaja <Laima.Duhovnaja@hiq.se>
2023-02-10 09:51:28 +0100

    Add script for importing data

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
@@ -14,6 +15,7 @@ mongoose
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
+    useUnifiedTopology: true,
   })
   .then(() => console.log('DB connection successful'));
 
