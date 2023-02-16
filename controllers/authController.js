445e5974758e52f9ce7b6d329e506d5874d6a05e
Laima Duhovnaja <Laima.Duhovnaja@hiq.se>
2023-02-16 14:07:01 +0100

    Add password reset functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -94,8 +94,9 @@ exports.protect = catchAsync(async (req, res, next) => {
   next();
 });
 
-exports.restrictTo = (...roles) => {
-  return (req, res, next) => {
+exports.restrictTo =
+  (...roles) =>
+  (req, res, next) => {
     // roles ['admin', 'lead-guide']. role='user'
     if (!roles.includes(req.user.role)) {
       return next(
@@ -105,4 +106,17 @@ exports.restrictTo = (...roles) => {
 
     next();
   };
-};
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  //1. Get user based on POSTed email
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(new AppError('There is no user with email address.', 404));
+  }
+  //2. Generate the random reset token
+  const resetToken = user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false });
+  //3. Send it to users email
+});
+
+exports.resetPassword = (req, res, next) => {};
